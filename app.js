a34b36d3f495b824b17afe4254164642fb638879
dipanshupandey <dipanshupandey4488@gmail.com>
2025-08-25 14:40:45 +0530

    episode 02

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
     </div>
 </div> */}
 
-const heading1=React.createElement("h1",{},"gruss gott");
-const heading2=React.createElement("h1",{},"auffweidersehen");
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const heading1=React.createElement("h1",{key:1},"gruss gott!!!");
+const heading2=React.createElement("h1",{key:2},"auffweidersehen");
 const child=React.createElement("div",{id:"child"},[heading1,heading2]);
 const parent=React.createElement("div",{id:"parent"},child);
 
