463311a19f7727e83272f842fa3b780a2da2169d
dipanshupandey <dipanshupandey4488@gmail.com>
2025-08-28 17:19:04 +0530

    Added React Hooks- Implemented Filter Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,21 @@
 import Card from "./Card"
 import { restraunts } from "../utils/mockdata"
+import { useState } from "react"
+
 const Body=()=>{
+    const [listOfRestraunt,setListOfRestaurants]=useState(restraunts);
     return <>            
                 <div className="body" >
                 <div className="search"> <input type="text" placeholder="search" /></div>
+                <button onClick={
+                    ()=>{
+                        const filteredRestraunts=listOfRestraunt.filter((restraunt)=>restraunt.info.avgRatingString>="4.4");
+                        setListOfRestaurants(filteredRestraunts);
+                    }
+                    } className="filterButton">
+                    Filter</button>
                 <div className="restrauntContainer">
-                {restraunts.map((res)=>{
+                {listOfRestraunt.map((res)=>{
                 return < Card info={res.info} key={res.info.id} />
                 })}
                   
