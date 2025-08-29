e328686c9b726af91784ebe985bd86d5c722e1a2
dipanshupandey <dipanshupandey4488@gmail.com>
2025-08-29 17:58:22 +0530

    Added React Hooks- Implemented search Functionality this alters the original data gotta fix it soon

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,19 @@ import { useState } from "react"
 
 const Body=()=>{
     const [listOfRestraunt,setListOfRestaurants]=useState(restraunts);
+    const [searchText,setSearchText]=useState("");
     return <>            
                 <div className="body" >
-                <div className="search"> <input type="text" placeholder="search" /></div>
+                <div className="search restraunt" onChange={(e)=>
+                    {
+                    
+                    setSearchText("e.target.value");
+                    const filteredRestraunts=listOfRestraunt.filter((restraunt)=>restraunt.info.name.toLowerCase().includes(e.target.value.toLowerCase()))
+                    
+                    setListOfRestaurants(filteredRestraunts);
+                    }
+                }> <input type="text" placeholder="search" /></div>
+                
                 <button onClick={
                     ()=>{
                         const filteredRestraunts=listOfRestraunt.filter((restraunt)=>restraunt.info.avgRatingString>="4.4");
@@ -14,8 +24,10 @@ const Body=()=>{
                     }
                     } className="filterButton">
                     Filter</button>
+
                 <div className="restrauntContainer">
                 {listOfRestraunt.map((res)=>{
+                    console.log(res.info)
                 return < Card info={res.info} key={res.info.id} />
                 })}
                   
