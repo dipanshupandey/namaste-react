23cf0556ebfaca73bc968e1ff346b27cab6c0905
dipanshupandey <dipanshupandey4488@gmail.com>
2025-08-30 17:20:13 +0530

    Added React Hooks- Used Use effect to render data from swiggy's api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,30 @@
 import Card from "./Card"
-import { restraunts } from "../utils/mockdata"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 const Body=()=>{
-    const [listOfRestraunt,setListOfRestaurants]=useState(restraunts);
+    const [listOfRestraunt,setListOfRestaurants]=useState([]);
     const [searchText,setSearchText]=useState("");
+    
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            let response = await fetch(
+              "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            );
+            let data = await response.json();
+            data=data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            setListOfRestaurants(data);
+            console.log(data);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+      
+        fetchData();
+      }, []); // empty dependency → runs only once on mount
+      
+
     return <>            
                 <div className="body" >
                 <div className="search restraunt" onChange={(e)=>
@@ -27,7 +47,7 @@ const Body=()=>{
 
                 <div className="restrauntContainer">
                 {listOfRestraunt.map((res)=>{
-                    console.log(res.info)
+                    // console.log(res.info)
                 return < Card info={res.info} key={res.info.id} />
                 })}
                   
